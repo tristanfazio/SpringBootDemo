aa1ea7516edaef7a2e4d1215001751a91e3f3a80
Tristan Fazio <2tfazio@iinet.net.au>
2020-10-04 22:49:49 +0800

    fix sorter test by implementing mergesort in sorter

--- src/main/java/dev/tristanfazio/SpringBootDemo/IntegerListSorter.java
+++ src/main/java/dev/tristanfazio/SpringBootDemo/IntegerListSorter.java
@@ -1,11 +1,55 @@
 package dev.tristanfazio.SpringBootDemo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class IntegerListSorter {
   public IntegerListSorter(){};
 
   public List<Integer> sortIntegerList(List<Integer> unsortedList) {
-    return null;
+    Integer[] listArray = unsortedList.toArray(Integer[]::new);
+
+    mergeSort(listArray, listArray.length);
+
+    return Arrays.asList(listArray);
+  }
+
+  private static void mergeSort(Integer[] unsortedArray, int arraySize) {
+    if(arraySize<2) {return;}
+
+    int middle = arraySize/2;
+    Integer[] left = new Integer[middle];
+    Integer[] right = new Integer[arraySize-middle];
+
+    for (int i = 0; i < middle; i++) {
+      left[i] = unsortedArray[i];
+    }
+    for (int i = middle; i < arraySize; i++){
+      right[i-middle] = unsortedArray[i];
+    }
+
+    mergeSort(left, middle);
+    mergeSort(right, arraySize-middle);
+
+    merge(unsortedArray,left,right,middle,arraySize-middle);
+  }
+
+  private static void merge(Integer[] unsortedArray, Integer[] left, Integer[] right, int middle, int offest) {
+    int i = 0, j = 0, k = 0;
+    while (i < middle && j < offest) {
+      if (left[i] <= right[j]) {
+        unsortedArray[k++] = left[i++];
+      }
+      else {
+        unsortedArray[k++] = right[j++];
+      }
+    }
+    while (i < middle) {
+      unsortedArray[k++] = left[i++];
+    }
+    while (j < offest) {
+      unsortedArray[k++] = right[j++];
+    }
   }
 }
--- src/test/java/dev/tristanfazio/SpringBootDemo/IntegerListSorterTests.java
+++ src/test/java/dev/tristanfazio/SpringBootDemo/IntegerListSorterTests.java
@@ -1,13 +1,27 @@
 package dev.tristanfazio.SpringBootDemo;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 public class IntegerListSorterTests {
+    List<Integer> unSortedList = Arrays.asList(
+            2, 4, 11, 1, 5, 13, 8, 15, 24, 19, 7, 17, 10, 14, 20, 16, 6, 25, 3, 18, 9, 22, 21, 23, 12
+    );
+
+    List<Integer> sortedList = Arrays.asList(
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
+    );
+
+    IntegerListSorter integerListSorter = new IntegerListSorter();
 
     @Test
     void givenAnUnsortedList_whenPassedToSorted_ThenSortedListIsReturned() {
-
+        List<Integer> actualSortedList = integerListSorter.sortIntegerList(unSortedList);
+        assertEquals(sortedList,actualSortedList);
     }
 }
