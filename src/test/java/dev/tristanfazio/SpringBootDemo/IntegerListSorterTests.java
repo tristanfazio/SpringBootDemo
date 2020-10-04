3da97d837a1cac112c3d0dbf04072eedf4eb730a
Tristan Fazio <2tfazio@iinet.net.au>
2020-10-04 21:51:22 +0800

    add test unit for sort

---
+++ src/test/java/dev/tristanfazio/SpringBootDemo/IntegerListSorterTests.java
@@ -0,0 +1,13 @@
+package dev.tristanfazio.SpringBootDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class IntegerListSorterTests {
+
+    @Test
+    void givenAnUnsortedList_whenPassedToSorted_ThenSortedListIsReturned() {
+
+    }
+}
