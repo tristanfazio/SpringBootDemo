576adcc3a08580ee124ddf1b97ac8541aa00ce4e
Tristan Fazio <2tfazio@iinet.net.au>
2020-10-04 23:01:58 +0800

    add summation and test

--- src/main/java/dev/tristanfazio/SpringBootDemo/IntegerListSum.java
+++ src/main/java/dev/tristanfazio/SpringBootDemo/IntegerListSum.java
@@ -1,2 +1,13 @@
-package dev.tristanfazio.SpringBootDemo;public class IntegerListSum {
+package dev.tristanfazio.SpringBootDemo;
+
+import java.util.List;
+
+public class IntegerListSum {
+    public IntegerListSum(){};
+
+    public int sum(List<Integer> integerList) {
+        return integerList.stream()
+            .mapToInt(Integer::intValue)
+            .sum();
+    }
 }
--- src/test/java/dev/tristanfazio/SpringBootDemo/IntegerListSumTests.java
+++ src/test/java/dev/tristanfazio/SpringBootDemo/IntegerListSumTests.java
@@ -1,2 +1,23 @@
-package dev.tristanfazio.SpringBootDemo;public class IntegerListSumTests {
+package dev.tristanfazio.SpringBootDemo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootTest
+public class IntegerListSumTests {
+
+    List<Integer> list = Arrays.asList(
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    );
+
+    IntegerListSum integerListSum = new IntegerListSum();
+
+    @Test
+    void givenAListOfIntegers_whenPassedToSummer_thenSummationReturned() {
+        assertEquals(55,integerListSum.sum(list));
+    }
 }
